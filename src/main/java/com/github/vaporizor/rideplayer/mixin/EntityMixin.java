c4a3fe7b5f5a4eb291d2eabe0220ba6e05140aad
vaporizor <159073161+vaporizor@users.noreply.github.com>
2024-04-10 17:27:57 +0300

    Modified signature

--- src/main/java/com/github/vaporizor/rideplayer/mixin/EntityMixin.java
+++ src/main/java/com/github/vaporizor/rideplayer/mixin/EntityMixin.java
@@ -4,15 +4,18 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAttachmentType;
 import net.minecraft.entity.EntityAttachments;
 import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@Debug(export=true)
 @Mixin(Entity.class)
 public class EntityMixin {
-    @Inject(method = "getPassengerAttachmentPos(Lnet/minecraft/entity/EntityL/net/minecraft/entity/EntityLnet/minecraft/entity/EntityAttachments;)L/net/minecraft/util/math/Vec3d;", at = @At("TAIL"))
-    private static Vec3d getPassengerAttachmentPos(Entity vehicle, Entity passenger, EntityAttachments attachments) {
+    @Inject(method = "getPassengerAttachmentPos(Lnet/minecraft/entity/Entity;L/net/minecraft/entity/Entity;Lnet/minecraft/entity/EntityAttachments;)L/net/minecraft/util/math/Vec3d;", at = @At("HEAD"), cancellable = true)
+    private static void getPassengerAttachmentPos(Entity vehicle, Entity passenger, EntityAttachments attachments, CallbackInfoReturnable<Vec3d> cir) {
         int i = vehicle.getPassengerList().indexOf(passenger);
-        return attachments.getPointOrDefault(EntityAttachmentType.PASSENGER, i, vehicle.getYaw()).subtract(0, 1, 0);
+        cir.setReturnValue(attachments.getPointOrDefault(EntityAttachmentType.PASSENGER, i, vehicle.getYaw()).subtract(0, 1, 0));
     }
 }
